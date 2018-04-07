c47eff36c66394f1e41951a4cdd17c933b205c98
cadoo-iscteiulpt <32901496+cadoo-iscteiulpt@users.noreply.github.com>
2018-04-07 15:31:33 +0100

    oiii
    oiii

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Citius RCC | Log in</title>
+  <title>mySatellite-IUL | Login</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
