8045fece25e48003425bbf43f3e98e99e1bc3593
Jorge Rafael Santos <jrcss11@iscte-iul.pt>
2018-04-07 14:59:28 +0100

    Base de dados

---
+++ database/config.php
@@ -0,0 +1,32 @@
+<?php
+
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PASS', '');
+	define('DB_NAME', 'mystaelliteiul');
+	
+	define('DB_CHAR', 'utf8');
+
+	
+	class Connection{
+		
+		protected $connection;
+		
+		public function Connection(){
+			
+			try{
+				
+				$this->connection= new PDO('mysql:host=' . DB_HOST . '; dbname=' . DB_NAME, DB_USER, DB_PASS);
+				
+				$this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				
+				$this->connection->exec("SET CHARACTER SET " . DB_CHAR);
+				return $this->connection;
+				
+			}catch(Exception $e){
+				echo "Erro na linha " . $e->getLine();	
+			}
+		}
+	}
+		
+?>
