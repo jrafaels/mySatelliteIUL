b84c7d1ddaaa5d9acd5884c942887495d1cd3296
Jorge Rafael Santos <jrcss11@iscte-iul.pt>
2018-04-09 19:42:51 +0100

    login

--- login.php
+++ login.php
@@ -1,57 +1,58 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<title> mySatellite-IUL | Login </title>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"> 
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-		<link rel="stylesheet" href="css/stylesheet.css" type="text/css">
-	</head>
+<head>
+<meta charset="utf-8">
+<title>mySatellite-IUL | Login</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="css/stylesheet.css" type="text/css">
+</head>
 
-	<body>
-	<?php
-			//session_start();
-			//$error=$_GET['q'];
-			$error="a";
-			if($error == "login"){
-				echo '<div class="alert alert-danger">
+<body>
+<?php
+			if(isset($_POST["enviar"])){
+			require("database/classes.php");
+		
+		try{
+			$user = new User();
+		
+			$username = htmlentities(addslashes($_POST['myusername']));
+			$password = htmlentities(addslashes($_POST['mypassword']));	
+		
+			$user->login($username,$password, $ip);
+			
+		}catch(Exception $e){
+			echo "Erro no login " . $e->GetMessage();	
+			echo '<div class="alert alert-danger">
 					  	<strong>Alert!</strong> Username and/or Password are not correct!
 					  	<button type="button" class="close" onclick="location.href=\'index.php\'">&times;</button>
 					</div>';
-			}
+		}
+	}
 	
 		?>
-		
-		<div class="top">
-			<img class="logo" src="">
-			
-			<?php
-				if(!isset($_SESSION['username'])){
-					echo  '<div class="login">
-						<h2 class="cover_text"> Bem Vindo </h2>
-						<form role="form" method="post" action="checklogin.php">
-						    <div class="form-group">
-						    	<label for="email">Username:</label>
-						     	<input name="myusername" type="text" class="form-control" id="email" placeholder="Introduzir username">
-						    </div>
-						    <div class="form-group">
-						      	<label for="pwd">Password:</label>
-						      	<input name="mypassword" type="password" class="form-control" id="pwd" placeholder="Introduzir password">
-						    </div>
-						    <button type="submit" class="btn btn-default">Enter</button>
-						</form>
-						</div>';
-				} else {
-					echo "<script>window.location = './main.php'</script>";
-					exit();
-				}
-			?>
-
-			<div class="footer">
-				<p id="footer_text">  ISCTE-IUL 2018 | SCDS | Professor: Francisco Cercas | Alunos: Carolina, Gonçalo e Jorge </p>
-			</div>
-		</div>
-	</body>
+<div class="top"> <img class="logo" src="">
+  <div class="login">
+    <h2 class="cover_text"> Bem Vindo </h2>
+    <form role="form" method="post" action="checklogin.php">
+      <div class="form-group">
+        <label for="email">Username:</label>
+        <input name="myusername" type="text" class="form-control" id="email" placeholder="Introduzir username">
+      </div>
+      <div class="form-group">
+        <label for="pwd">Password:</label>
+        <input name="mypassword" type="password" class="form-control" id="pwd" placeholder="Introduzir password">
+      </div>
+      <button type="submit" name="enviar" class="btn btn-default">Enter</button>
+    </form>
+  </div>
+  <!--echo "<script>window.location = './main.php'</script>";-->
+  
+  <div class="footer">
+    <p id="footer_text"> ISCTE-IUL 2018 | SCDS | Professor: Francisco Cercas | Alunos: Carolina, Gonçalo e Jorge </p>
+  </div>
+</div>
+</body>
 </html>
