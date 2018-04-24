4e8ef3feb45456e590271f7fa3116df651f0d395
Gonçalo Simões <garss@iscte-iul.pt>
2018-04-24 15:02:10 +0100

    fodam-se todos

--- perfil_satelite.php
+++ perfil_satelite.php
@@ -61,51 +61,82 @@
     </table>
   </div>
   <div class="mainTLE">
-    <table class=tableTLE>
-      <!-- TLE -->
-      <tr>
-        <td>Raio do Perigeu (rp):</td>
-        <td><?php echo $sat->getPerigeeRadius() ?> km</td>
-        <td>Coordenadas do Satélite</td>
-        <td> </td>
-      </tr>
-      <tr>
-        <td>Raio do Apogeu (ra):</td>
-        <td><?php echo $sat->getApogeeRadius() ?> km</td>
-        <td>  Latitude:</td>
-        <td>RAFA PREENCHE</td>
-      </tr>
-      <tr>
-        <td>Excentricidade (e):</td>
-        <td><?php echo $sat->getExcentricity() ?></td>
-        <td>  Longitude:</td>
-        <td>RAFA PREENCHE</td>
-      </tr>
-      <tr>
-        <td>Semi-Eixo Menor(b):</td>
-        <td><?php echo $sat->getSemiMinor() ?></td>
-        <td>  Altitude:</td>
-        <td>RAFA PREENCHE</td>
-      </tr>
-      <tr>
-        <td>Azimute (az):</td>
-        <td><?php echo $sat->getAzimute() ?></td>
-        <td>Coordenadas do ISCTE</td>
-        <td> </td>
-      </tr>
-      <tr>
-        <td>Elevação:</td>
-        <td><?php echo $sat->getElevation() ?></td>
-        <td>  Latitude:</td>
-        <td>RAFA PREENCHE</td>
-      </tr>
-      <tr>
-        <td>  </td>
-        <td>  </td>
-        <td>  Longitude:</td>
-        <td>RAFA PREENCHE</td>
-      </tr>
-    </table>
+
+    <div class="dataTLE">
+      <table class=tableTLE1>
+        <!-- TLE -->
+        <tr>
+          <td><b>TLE</b></td>
+          <td>RAFAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</td>
+        </tr>
+      </table>
+    </div>
+
+    <div class="TLE">
+      <table class=tableTLE2>
+        <!-- TLE -->
+        <tr>
+          <td>Raio do Perigeu (rp):</td>
+          <td><?php echo $sat->getPerigeeRadius() ?> km</td>
+        </tr>
+        <tr>
+          <td>Raio do Apogeu (ra):</td>
+          <td><?php echo $sat->getApogeeRadius() ?> km</td>
+        </tr>
+        <tr>
+          <td>Excentricidade (e):</td>
+          <td><?php echo $sat->getExcentricity() ?></td>
+        </tr>
+        <tr>
+          <td>Semi-Eixo Menor(b):</td>
+          <td><?php echo $sat->getSemiMinor() ?></td>
+        </tr>
+        <tr>
+          <td>Azimute (az):</td>
+          <td><?php echo $sat->getAzimute() ?></td>
+        </tr>
+        <tr>
+          <td>Elevação:</td>
+          <td><?php echo $sat->getElevation() ?></td>
+        </tr>
+
+      </table>
+    </div>
+
+    <div class="coordenadas">
+      <table class=tableCoordenadas>
+        <!-- TLE -->
+        <tr>
+          <td>Coordenadas do Satélite</td>
+          <td> </td>
+        </tr>
+        <tr>
+          <td>  Latitude:</td>
+          <td>RAFA PREENCHE</td>
+        </tr>
+        <tr>
+          <td>  Longitude:</td>
+          <td>RAFA PREENCHE</td>
+        </tr>
+        <tr>
+          <td>  Altitude:</td>
+          <td>RAFA PREENCHE</td>
+        </tr>
+        <tr>
+          <td>Coordenadas do ISCTE</td>
+          <td> </td>
+        </tr>
+        <tr>
+          <td>  Latitude:</td>
+          <td>RAFA PREENCHE</td>
+        </tr>
+        <tr>
+          <td>  Longitude:</td>
+          <td>RAFA PREENCHE</td>
+        </tr>
+      </table>
+    </div>
+
   </div>
     <div class="media">
       <div class="image">
