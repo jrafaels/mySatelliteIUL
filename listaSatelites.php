e1a1ffbd30c6d9981385cb977455ce621b869fc0
cadoo-iscteiulpt <32901496+cadoo-iscteiulpt@users.noreply.github.com>
2018-04-24 11:49:31 +0100

    loladas

---
+++ listaSatelites.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<title> mySatellite-IUL</title>
+	<link rel="stylesheet" href="css/stylesheet.css" type="text/css">
+	<meta charset="utf-8">
+</head>
+
+<body>
+<?php
+	include('cabecalho.php');
+	include('menu.php');
+?>
+	<div class="main">
+		<table class="tableLista">
+     		<tr id= tituloLista>
+        		<td>Nome do Satélite:</td>
+      		</tr>
+		     <tr>
+		      	<td>OI</td>
+		      </tr>
+		      <tr>
+		      	<td>OI outra vez</td>
+		      </tr>
+
+     	</table>
+	</div>
+</body>
+</html>
+
